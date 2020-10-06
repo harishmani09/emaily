d9847a11d30d3e8d9106e3f253ffc89c0520ce5c
harishmani09 <34627727+harishmani09@users.noreply.github.com>
2020-10-06 06:41:33 +0530

    new key

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -9,7 +9,11 @@ const Survey = mongoose.model('surveys');
 
 module.exports = app => {
 
-    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    app.get('/api/surveys/thanks', (req,res) => {
+        res.send('Thanks for your valuable feedback');
+    })
+
+    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
 
         const { title, subject, body, recipients } = req.body;
 
@@ -23,9 +27,17 @@ module.exports = app => {
         });
 
         const mailer = new Mailer(survey, surveyTemplate(survey));
-        mailer.send();
 
-
+        try {
+            await mailer.send();
+            await survey.save();
+            req.user.credits -= 1;
+            const user = await req.user.save();
+            
+            res.send(user);
+        } catch (err) {
+            res.status(422).send(err);
+        }
 
     });
 
--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,5 +1,26 @@
+const keys = require('../../config/keys');
+
 module.exports = (survey) => {
 
-return '<div>' + survey.body + '<div>';
+return `
+
+    <html>
+        <body>
+            <div style="text-align: center;">
+            <h3> I would appreciate your inputs</h3>
+            <p> pls answer the followign questions:  </p>
+            <p> ${survey.body}  </p>
+            <div>
+                <a href= "${keys.redirectDomain}/api/surveys/thanks" > Yes </a>
+                
+            </div>
+            <div>
+            <a href= "${keys.redirectDomain}/api/surveys/thanks" > No </a>
+            </div>
+
+            </div>
+        </body>
+    </html>
+`;
 
 };
